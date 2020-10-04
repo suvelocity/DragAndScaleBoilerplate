e22c0db2570dd23a60626e6e468b6f0acc86cdef
Nitzan Listman <67858069+listguy@users.noreply.github.com>
2020-10-04 11:15:38 +0300

    Initial commit

---
+++ tests/packageTest.test.js
@@ -0,0 +1,13 @@
+test("package.json does not include any extra libraries ", () => {
+  const pack = require("../package.json");
+  const expectedDependencies = ["jest", "puppeteer"];
+
+  const isDependeciesClean = Object.keys(pack.dependencies)
+    .map((key) => expectedDependencies.includes(key))
+    .every((result) => result === true);
+
+  const isDevDependeciesClean = pack.devDependencies === undefined;
+
+  expect(isDependeciesClean).toBe(true);
+  expect(isDevDependeciesClean).toBe(true);
+});
